f2a001662f007f93d16882d0e75788874c72a031
NarutoGM <t1053300121@unitru.edu.pe>
2026-09-21 23:41:32 -0500

    feat(deploy): configure vercel.json, enable CORS, and prisma generate on build

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
